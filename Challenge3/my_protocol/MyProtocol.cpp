f299eca539cb2bdb078b72950606ba026f340c58
Sönke van Loh <s.u.vanloh@student.utwente.nl>
2021-02-22 12:27:30 +0100

    soenkes final sol

--- Challenge3/my_protocol/MyProtocol.cpp
+++ Challenge3/my_protocol/MyProtocol.cpp
@@ -23,20 +23,187 @@ namespace my_protocol {
     MyProtocol::MyProtocol() {
         unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count(); // This grabs the lower 32 bits for our seed.
         this->rnd = std::default_random_engine(seed);
-		cnt = 0;
 		MyserialNumber = this->rnd() % 1000000;
 		std::cout << MyserialNumber << std::endl;
-		collisionHappend = sendSerial = trySending = false;
-		waitFor = 0;
+		transmitting = false;
+		cnt = 0;
+		collisions = 0;
     }
 
     MyProtocol::~MyProtocol() {
     }
 
-    TransmissionInfo MyProtocol::TimeslotAvailable(
-        MediumState previousMediumState, int32_t controlInformation, int localQueueLength) {
-        //check if there is a new user that needs to be added
-		if (controlInformation > 0 && controlInformation < queMultiplyer) {
+	TransmissionInfo MyProtocol::TimeslotAvailable(
+		MediumState previousMediumState, int32_t controlInformation, int localQueueLength) {
+		//std::cout << cnt << ": " << std::endl;
+		cnt++;
+		switch (previousMediumState) {
+		case 1: //there was a collision in the last frame
+		{
+			collisions++;
+				bool inque = false;
+				if (que.size()) {
+					int beginning = que.front();
+					do {
+						if (MyserialNumber == que.front())
+							inque = true;
+						que.push(que.front());
+						que.pop();
+					} while (que.front() != beginning);
+				}
+				if (!inque && localQueueLength) {
+					//std::cout << "coll1" << std::endl;
+					if (collisions < 2) {
+						//std::cout << "SLOT - tryig to send Data because I want to get into the queue " << MyserialNumber << " turn." << std::endl;
+						return TransmissionInfo{ Data, encodecontroleInfo(MyserialNumber, add) };
+					}
+					else {
+						if (this->rnd() % 100 < 60) {
+							//std::cout << "SLOT - trying to send data by chance." << std::endl;
+							return TransmissionInfo{ Data, add };
+						}
+						else {
+							//std::cout << "SLOT - Not sending data to let new sender into queue (because it was not my lucky day)." << std::endl;
+							return TransmissionInfo{ Silent, 0 };
+						}
+					}
+				}
+				else {
+					//std::cout << "SLOT - Not sending data to let new sender into queue." << std::endl;
+					return TransmissionInfo{ Silent, 0 };
+				}
+			break;
+		}
+		case 2: //there was successfull transmission in the last frame
+		{
+			collisions = 0;
+			int32_t serial = decodeSerial(controlInformation);
+			statusBit status = decodestatusBit(controlInformation);
+			//std::cout << "The status bit is " << status << " and the serial number of the sender is: " << serial << std::endl;
+			if (status == add) {
+				que.push(serial);
+				if (serial == MyserialNumber) {
+				transmitting = true;
+				}
+				//std::cout << "pushed " << serial << " in queue" << std::endl;
+			}
+			else if (status == finish) {
+				if (serial == MyserialNumber) {
+					transmitting = false;
+				}
+				//std::cout << "finish 1" << std::endl;
+				if (que.front() == serial) {
+					que.pop();
+					//std::cout << "finish2" << std::endl;
+				}
+				else {
+					//std::cout << "finish3" << std::endl;
+					int beginning = que.front();
+					do {
+						if (que.front() != serial) que.push(que.front());
+						que.pop();
+					} while (que.front() != beginning);
+				}
+			}
+			if (que.size()) {
+				int currentSerial = que.front();
+				que.pop();
+				que.push(currentSerial);
+				//std::cout << "transmitting: " << transmitting << std::endl;
+				if (!transmitting && localQueueLength) {
+					//std::cout << "SLOT - tryig to send Data because I want to get into the queue " << MyserialNumber << " turn." << std::endl;
+					return TransmissionInfo{ Data, encodecontroleInfo(MyserialNumber, add) };
+				}
+				if (currentSerial == MyserialNumber) {
+					if (localQueueLength) {
+						//std::cout << "SLOT - tryig to send Data because last transmission was successfull and it is " << MyserialNumber << " turn." << std::endl;
+						return TransmissionInfo{ Data, encodecontroleInfo(MyserialNumber, ok) };
+					}
+					else {
+						//std::cout << "SLOT - tryig to send finish because last transmission was successfull and it is " << MyserialNumber << " turn." << std::endl;
+						return TransmissionInfo{ NoData, encodecontroleInfo(MyserialNumber, finish) };
+					}
+				}
+				else {
+					//std::cout << "SLOT - Not sending data to give room for others." << std::endl;
+					return TransmissionInfo{ Silent, 0 };
+				}
+			}
+			else {
+				//std::cout << "SLOT - No Data to Send as que is empty." << std::endl;
+				return TransmissionInfo{ Silent, 0 };
+			}
+			break;
+		}
+		default: //includes case 0 just done this way as I dont exactly know what the initial controlInfo is
+		{
+			collisions = 0;
+			if (localQueueLength) {
+				//std::cout << "SLOT - tryig to send Data because channel was idle." << std::endl;
+				return TransmissionInfo{ Data, encodecontroleInfo(MyserialNumber, add) };
+			}
+			else {
+				//std::cout << "SLOT - Not sending data as que is empty." << std::endl;
+				return TransmissionInfo{ Silent, 0 };
+			}
+			break;
+		}
+
+
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+	int32_t MyProtocol::encodecontroleInfo(int32_t Serial, statusBit status)  
+	{
+		return Serial + (int32_t)status*queMultiplyer;
+	}
+	int32_t MyProtocol::decodeSerial(int32_t controleInfo)
+	{
+		int temp = controleInfo / queMultiplyer;
+		if (temp < 100) return controleInfo;
+		else if (temp < 500) return controleInfo - 100*queMultiplyer;
+		else return controleInfo - 1000*queMultiplyer;
+	}
+
+	MyProtocol::statusBit MyProtocol::decodestatusBit(int32_t controleInfo)
+	{
+		int temp = controleInfo / queMultiplyer;
+		if (temp < 100) return ok;
+		else if (temp < 500) return add;
+		else return finish;
+	}
+		//check if there is a new user that needs to be added
+		/*if (controlInformation > 0 && controlInformation < queMultiplyer) {
 			user tempUser;
 			tempUser.serialNumber = controlInformation;
 			userList.emplace_back(tempUser);
@@ -109,8 +276,9 @@ namespace my_protocol {
 				return TransmissionInfo{ Silent, 0 };
 			}
 		}
-
-    }
+		
+		
+		}*/
 
 	
 
--- Challenge3/my_protocol/MyProtocol.h
+++ Challenge3/my_protocol/MyProtocol.h
@@ -21,6 +21,7 @@
 #include <random>
 #include <iostream>
 #include <vector> 
+#include <queue>
 #include <string>
 
 #ifndef PROTOCOL_MYPROTOCOL_H_
@@ -35,15 +36,17 @@ namespace my_protocol {
         TransmissionInfo TimeslotAvailable(MediumState previousMediumState, int32_t controlInformation, int localQueueLength);
     private:
         std::default_random_engine rnd;
-		int cnt, waitFor;
 		int32_t MyserialNumber;
-		bool sendSerial, trySending, collisionHappend;
+		int cnt, collisions;
+		bool transmitting;
 		const int32_t queMultiplyer= pow(2, 20);
-		struct user {
-			int32_t serialNumber = 0;
-			bool que = false;
+		std::queue<int32_t> que;
+		enum statusBit {
+			add = 100, ok = 0, finish = 1000
 		};
-		std::vector<user> userList;
+		int32_t encodecontroleInfo(int32_t, statusBit); //encodes the serial with the status info for the controle information field
+		statusBit decodestatusBit(int32_t); //decodes controle info field and returns the status
+		int32_t decodeSerial(int32_t); // decodes controle info field and returns the senders address
     };
 
 } /* namespace my_protocol */
