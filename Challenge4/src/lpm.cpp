d83537a447332219262f52e1ed028904fbac1a17
ICheered <tjeerd992@gmail.com>
2021-03-04 13:33:44 +0100

    Insert node

--- Challenge4/src/lpm.cpp
+++ Challenge4/src/lpm.cpp
@@ -39,59 +39,6 @@ struct best_match {
     best_match(int8_t pl, int32_t ref, int16_t pn) : pl(pl), ref(ref), pn(pn) {}
 };
 
-std::vector<ip_entry> ip_entrys;
-int reftemp;
-
-
-
-void init() {
-    //read in the txt into data struct
-   // You can use this function to initialize variables.
-    ip_entrys.reserve(420972);
-}
-
-void add_route(unsigned int ip, int prefix_length, int port_number) {
-    // TODO: Store route to be used for later use in lookup_ip() function
-    ip_entrys.emplace_back(prefix_length, ip, port_number);
-}
-
-// This method is called after all routes have been added.
-// You don't have to use this method but can use it to sort or otherwise
-// organize the routing information, if your datastructure requires this.
-void finalize_routes() {
-    // TODO: (optionally) use this to finalize your routes.
-}
-
-int lookup_ip(unsigned int ip) {
-    // TODO: Lookup IP in stored data from add_route function,
-    //       returns port number (or -1 for no route found).
-    best_match bestie(0, 0, -1); //initiate best match
-    for (const auto& iterator : ip_entrys) {
-        reftemp = iterator.ip ^ ip;
-        reftemp = reftemp >> (32 - iterator.pl);
-        if (reftemp == 0 && bestie.pl < iterator.pl) {
-                bestie.pl = iterator.pl;
-                bestie.ref = reftemp;
-                bestie.pn = iterator.pn;
-        }
-    }
-    return bestie.pn;
-}
-
-
-// convenience function to print IP addresses in human readable form for debugging
-void ip2human(unsigned int ip) {
-    unsigned int a, b, c, d;
-
-    a = (ip >> 24) & 0xff;
-    b = (ip >> 16) & 0xff;
-    c = (ip >>  8) & 0xff;
-    d =  ip        & 0xff;
-
-    printf("%i.%i.%i.%i\n", a, b, c, d);
-}
-
-
 
 
 
@@ -127,15 +74,15 @@ public:
 
     void insertNode(unsigned int ip, unsigned int remaining_prefix, int port) //  IP, Prefix Length, Port
     {
-        insertNodeHelper(&rootPtr, ip, remaining_prefix, port);
+        Tree::insertNodeHelper(&rootPtr, ip, remaining_prefix, port);
     }
     void getPort(unsigned int ip)
     {
-        return getPortHelper(&rootPtr, ip);
+        return Tree::getPortHelper(&rootPtr, ip);
     }
-};
 
-void insertNodeHelper(TreeNode **ptr, unsigned int ip, unsigned int remaining_prefix, int port)
+
+    void insertNodeHelper(TreeNode **ptr, unsigned int ip, unsigned int remaining_prefix, int port)
     {
 
         // If remaining_prefix == 0
@@ -183,16 +130,91 @@ void insertNodeHelper(TreeNode **ptr, unsigned int ip, unsigned int remaining_pr
         }
     } 
 
-void getPortHelper(TreeNode **ptr, unsigned int inverted_ip)
-{
-    // If current IP bit == 0 
-        // If current node leftptr == nullptr
-            // Return current node port
-        // Else 
-            // Return getPortHelper (current node leftptr, inverted_ip rightshifted)
-    // Else If current IP bit == 1 
-        // If current node rightptr == nullptr
-            // Return current node port
-        // Else 
-            // Return getPortHelper (current node rightptr, inverted_ip rightshifted)
+    void getPortHelper(TreeNode **ptr, unsigned int inverted_ip)
+    {
+        // If current IP bit == 0 
+            // If current node leftptr == nullptr
+                // Return current node port
+            // Else 
+                // Return getPortHelper (current node leftptr, inverted_ip rightshifted)
+        // Else If current IP bit == 1 
+            // If current node rightptr == nullptr
+                // Return current node port
+            // Else 
+                // Return getPortHelper (current node rightptr, inverted_ip rightshifted)
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+std::vector<ip_entry> ip_entrys;
+int reftemp;
+
+Tree portTree;
+
+
+void init() {
+    //read in the txt into data struct
+   // You can use this function to initialize variables.
+    ip_entrys.reserve(420972);
 }
+
+void add_route(unsigned int ip, int prefix_length, int port_number) {
+    // TODO: Store route to be used for later use in lookup_ip() function
+    //ip_entrys.emplace_back(prefix_length, ip, port_number);
+    portTree.insertNode(ip, prefix_length, port_number);
+}
+
+// This method is called after all routes have been added.
+// You don't have to use this method but can use it to sort or otherwise
+// organize the routing information, if your datastructure requires this.
+void finalize_routes() {
+    // TODO: (optionally) use this to finalize your routes.
+}
+
+int lookup_ip(unsigned int ip) {
+    // TODO: Lookup IP in stored data from add_route function,
+    //       returns port number (or -1 for no route found).
+    best_match bestie(0, 0, -1); //initiate best match
+    for (const auto& iterator : ip_entrys) {
+        reftemp = iterator.ip ^ ip;
+        reftemp = reftemp >> (32 - iterator.pl);
+        if (reftemp == 0 && bestie.pl < iterator.pl) {
+                bestie.pl = iterator.pl;
+                bestie.ref = reftemp;
+                bestie.pn = iterator.pn;
+        }
+    }
+    return bestie.pn;
+}
+
+
+// convenience function to print IP addresses in human readable form for debugging
+void ip2human(unsigned int ip) {
+    unsigned int a, b, c, d;
+
+    a = (ip >> 24) & 0xff;
+    b = (ip >> 16) & 0xff;
+    c = (ip >>  8) & 0xff;
+    d =  ip        & 0xff;
+
+    printf("%i.%i.%i.%i\n", a, b, c, d);
+}
+
+
